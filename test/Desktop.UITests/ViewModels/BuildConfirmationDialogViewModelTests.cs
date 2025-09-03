852afee26436ac2eae8d9fee34d63a014c872620
Heikki-Jussi Niemi <hekku2@gmail.com>
2025-09-03 18:44:05 +0300

    Improve tests

--- test/Desktop.UITests/ViewModels/BuildConfirmationDialogViewModelTests.cs
+++ test/Desktop.UITests/ViewModels/BuildConfirmationDialogViewModelTests.cs
@@ -38,7 +38,7 @@ public class BuildConfirmationDialogViewModelTests
         return new BuildConfirmationDialogViewModel(optionsToUse, _mockFileCollector, _mockCombination, _mockFileWriter, _mockFileService, _mockLogger);
     }
 
-    private static async Task WaitForConditionAsync(System.Func<bool> condition, int timeoutMs = 2000, int intervalMs = 10)
+    private static async Task WaitForConditionAsync(Func<bool> condition, int timeoutMs = 2000, int intervalMs = 10)
     {
         var maxWait = timeoutMs / intervalMs;
         var waitCount = 0;
@@ -52,20 +52,20 @@ public class BuildConfirmationDialogViewModelTests
     [AvaloniaTest]
     public void BuildConfirmationDialog_Should_Show_Combined_Project_And_Output_Path()
     {
-        var customOptions = new ApplicationOptions 
-        { 
+        var customOptions = new ApplicationOptions
+        {
             DefaultProjectFolder = "/test/project",
-            DefaultOutputFolder = "test-output" 
+            DefaultOutputFolder = "test-output"
         };
-        
+
         var dialogViewModel = CreateDialogViewModel(customOptions);
-        
+
         Assert.Multiple(() =>
         {
             Assert.That(dialogViewModel.OutputLocation, Is.Not.Null.And.Not.Empty, "Output location should be set");
             Assert.That(dialogViewModel.OutputLocation, Is.EqualTo("/test/project/test-output"), "Should combine project folder and output folder");
-            Assert.That(dialogViewModel.OutputLocation, Does.StartWith("/test/project"), "Should start with project folder");
-            Assert.That(dialogViewModel.OutputLocation, Does.EndWith("test-output"), "Should end with output folder");
+            Assert.That(dialogViewModel.OutputLocation.Replace('\\', '/'), Does.StartWith("/test/project"), "Should start with project folder");
+            Assert.That(dialogViewModel.OutputLocation.Replace('\\', '/'), Does.EndWith("test-output"), "Should end with output folder");
         });
     }
 
@@ -73,7 +73,7 @@ public class BuildConfirmationDialogViewModelTests
     public void BuildConfirmationDialog_Should_Have_Cancel_And_Compile_Commands()
     {
         var dialogViewModel = CreateDialogViewModel();
-        
+
         Assert.Multiple(() =>
         {
             Assert.That(dialogViewModel.CancelCommand, Is.Not.Null, "Cancel command should exist");
@@ -87,14 +87,14 @@ public class BuildConfirmationDialogViewModelTests
     public void BuildConfirmationDialog_Should_Trigger_DialogClosed_Event_On_Cancel()
     {
         var dialogViewModel = CreateDialogViewModel();
-        
+
         // Track if dialog closed event was triggered
         bool dialogClosed = false;
         dialogViewModel.DialogClosed += (sender, e) => dialogClosed = true;
-        
+
         // Execute the cancel command
         dialogViewModel.CancelCommand.Execute(null);
-        
+
         Assert.That(dialogClosed, Is.True, "DialogClosed event should be triggered when Cancel command is executed");
     }
 
@@ -124,7 +124,7 @@ public class BuildConfirmationDialogViewModelTests
                 Content = "# Template 2"
             }
         };
-        
+
         var sourceFiles = new[]
         {
             new MarkdownDocument
@@ -168,7 +168,7 @@ public class BuildConfirmationDialogViewModelTests
 
         // Act
         dialogViewModel.SaveCommand.Execute(null);
-        
+
         // Wait for async build to complete
         await WaitForConditionAsync(() => !dialogViewModel.IsBuildInProgress, 1000);
 
@@ -184,6 +184,7 @@ public class BuildConfirmationDialogViewModelTests
             _mockCombination.Received(1).Validate(Arg.Any<IEnumerable<MarkdownDocument>>(), Arg.Any<IEnumerable<MarkdownDocument>>());
             _mockCombination.Received(1).BuildDocumentation(templateFiles, sourceFiles);
             _mockFileWriter.Received(1).WriteDocumentsToFolderAsync(processedDocuments, "/test/project/output");
+            _mockFileService.DidNotReceive().DeleteFolderContentsAsync(Arg.Any<string>());
         });
     }
 
@@ -206,7 +207,7 @@ public class BuildConfirmationDialogViewModelTests
 
         // Act
         dialogViewModel.SaveCommand.Execute(null);
-        
+
         // Wait for async build to complete
         await WaitForConditionAsync(() => !dialogViewModel.IsBuildInProgress, 1000);
 
@@ -252,6 +253,113 @@ public class BuildConfirmationDialogViewModelTests
             Assert.That(dialogViewModel.IsBuildInProgress, Is.False, "Build should not be in progress initially");
             Assert.That(dialogViewModel.CanBuild, Is.True, "Should be able to build when not in progress");
             Assert.That(dialogViewModel.SaveCommand.CanExecute(null), Is.True, "Compile command should be enabled when build is not in progress");
+        });
+    }
+
+    [AvaloniaTest]
+    public async Task BuildConfirmationDialog_Should_Clean_Output_Before_Writing_When_CleanOld_Enabled()
+    {
+        var customOptions = new ApplicationOptions
+        {
+            DefaultProjectFolder = "/test/project",
+            DefaultOutputFolder = "output"
+        };
+
+        var templateFiles = new[]
+        {
+            new MarkdownDocument { FileName = "t.mdext", FilePath = "/t.mdext", Content = "# T <insert s.mdsrc>" }
+        };
+        var sourceFiles = new[]
+        {
+            new MarkdownDocument { FileName = "s.mdsrc", FilePath = "/s.mdsrc", Content = "S" }
+        };
+        var processedDocuments = new[]
+        {
+            new MarkdownDocument { FileName = "t.md", FilePath = "/t.md", Content = "# T\nS" }
+        };
+
+        _mockFileCollector.CollectAllMarkdownFilesAsync("/test/project").Returns((templateFiles, sourceFiles));
+        _mockCombination.Validate(Arg.Any<IEnumerable<MarkdownDocument>>(), Arg.Any<IEnumerable<MarkdownDocument>>())
+            .Returns(new ValidationResult());
+        _mockCombination.BuildDocumentation(templateFiles, sourceFiles).Returns(processedDocuments);
+        _mockFileService.DeleteFolderContentsAsync("/test/project/output").Returns(true);
+        _mockFileWriter.WriteDocumentsToFolderAsync(processedDocuments, "/test/project/output").Returns(Task.CompletedTask);
+
+        var viewModel = CreateDialogViewModel(customOptions);
+        viewModel.CleanOld = true;
+
+        viewModel.SaveCommand.Execute(null);
+        await WaitForConditionAsync(() => !viewModel.IsBuildInProgress, 3000);
+
+        Assert.Multiple(() =>
+        {
+            _mockFileService.Received(1).DeleteFolderContentsAsync("/test/project/output");
+            _mockFileWriter.Received(1).WriteDocumentsToFolderAsync(processedDocuments, "/test/project/output");
+        });
+    }
+
+    [AvaloniaTest]
+    public async Task BuildConfirmationDialog_Should_Not_Clean_When_CleanOld_Disabled()
+    {
+        var customOptions = new ApplicationOptions
+        {
+            DefaultProjectFolder = "/test/project",
+            DefaultOutputFolder = "output"
+        };
+
+        var templateFiles = new[] { new MarkdownDocument { FileName = "t.mdext", FilePath = "/t.mdext", Content = "# T" } };
+        var sourceFiles = new[] { new MarkdownDocument { FileName = "s.mdsrc", FilePath = "/s.mdsrc", Content = "S" } };
+        var processedDocuments = new[] { new MarkdownDocument { FileName = "t.md", FilePath = "/t.md", Content = "# T" } };
+
+        _mockFileCollector.CollectAllMarkdownFilesAsync("/test/project").Returns((templateFiles, sourceFiles));
+        _mockCombination.Validate(Arg.Any<IEnumerable<MarkdownDocument>>(), Arg.Any<IEnumerable<MarkdownDocument>>())
+            .Returns(new ValidationResult());
+        _mockCombination.BuildDocumentation(templateFiles, sourceFiles).Returns(processedDocuments);
+        _mockFileWriter.WriteDocumentsToFolderAsync(processedDocuments, "/test/project/output").Returns(Task.CompletedTask);
+
+        var viewModel = CreateDialogViewModel(customOptions);
+        viewModel.CleanOld = false; // explicit
+
+        viewModel.SaveCommand.Execute(null);
+        await WaitForConditionAsync(() => !viewModel.IsBuildInProgress, 3000);
+
+        Assert.Multiple(() =>
+        {
+            _mockFileService.DidNotReceive().DeleteFolderContentsAsync(Arg.Any<string>());
+            _mockFileWriter.Received(1).WriteDocumentsToFolderAsync(processedDocuments, "/test/project/output");
+        });
+    }
+
+    [AvaloniaTest]
+    public async Task BuildConfirmationDialog_Should_Proceed_When_Clean_Fails()
+    {
+        var customOptions = new ApplicationOptions
+        {
+            DefaultProjectFolder = "/test/project",
+            DefaultOutputFolder = "output"
+        };
+
+        var templateFiles = new[] { new MarkdownDocument { FileName = "t.mdext", FilePath = "/t.mdext", Content = "# T" } };
+        var sourceFiles = new[] { new MarkdownDocument { FileName = "s.mdsrc", FilePath = "/s.mdsrc", Content = "S" } };
+        var processedDocuments = new[] { new MarkdownDocument { FileName = "t.md", FilePath = "/t.md", Content = "# T" } };
+
+        _mockFileCollector.CollectAllMarkdownFilesAsync("/test/project").Returns((templateFiles, sourceFiles));
+        _mockCombination.Validate(Arg.Any<IEnumerable<MarkdownDocument>>(), Arg.Any<IEnumerable<MarkdownDocument>>())
+            .Returns(new ValidationResult());
+        _mockCombination.BuildDocumentation(templateFiles, sourceFiles).Returns(processedDocuments);
+        _mockFileService.DeleteFolderContentsAsync("/test/project/output").Returns(false); // simulate failure
+        _mockFileWriter.WriteDocumentsToFolderAsync(processedDocuments, "/test/project/output").Returns(Task.CompletedTask);
+
+        var viewModel = CreateDialogViewModel(customOptions);
+        viewModel.CleanOld = true;
+
+        viewModel.SaveCommand.Execute(null);
+        await WaitForConditionAsync(() => !viewModel.IsBuildInProgress, 3000);
+
+        Assert.Multiple(() =>
+        {
+            _mockFileService.Received(1).DeleteFolderContentsAsync("/test/project/output");
+            _mockFileWriter.Received(1).WriteDocumentsToFolderAsync(processedDocuments, "/test/project/output");
         });
     }
 }
